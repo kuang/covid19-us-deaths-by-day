670fed8e2b9a8e5b0aee8fc2152fa6e3e5daa81e
Justin Kuang <zk65@cornell.edu>
2020-10-30 16:53:45 -0400

    monthly segments

--- data.js
+++ data.js
@@ -1,15 +1,28 @@
 const secondsInDay = 86400;
-const DOT_SIZE = 3;
-const SPACING_SIZE = 13;
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const DOT_SIZE = 2;
+const SPACING_SIZE = 10;
 
-let canvasWidth = canvas.clientWidth;
-canvas.width = canvasWidth;
+let monthlyDeathData = [
+    { month: "feburary", deaths: 8 },
+    { month: "march", deaths: 5324 },
+    { month: "april", deaths: 61356 },
+    { month: "may", deaths: 101163 },
+    { month: "june", deaths: 120762 },
+    { month: "july", deaths: 146537 },
+    { month: "august", deaths: 176639 },
+    { month: "september", deaths: 199776 },
+    { month: "october", deaths: 0 },
+];
+// let canvasWidth = canvas.clientWidth;
+// canvas.width = canvasWidth;
 d3.json("https://api.covidtracking.com/v1/us/current.json").then(data => {
     let dateStringInfo = getDateStringFromDateInt(data[0].date);
     let dailyDeaths = data[0].deathIncrease;
     let totalDeaths = data[0].death;
+    monthlyDeathData[monthlyDeathData.length - 1].deaths = totalDeaths;
+    for (let i = monthlyDeathData.length - 1; i > 0; i--) {
+        monthlyDeathData[i].deaths = monthlyDeathData[i].deaths - monthlyDeathData[i - 1].deaths;
+    }
     document.getElementById("title").innerHTML =
         "".concat(
             dateStringInfo,
@@ -20,34 +33,42 @@ d3.json("https://api.covidtracking.com/v1/us/current.json").then(data => {
     document.getElementById("total_deaths").innerHTML =
         "".concat(
             'There have been ',
-            totalDeaths,
-            ' COVID-19 deaths in the US since January 2020. What does this toll actually look like?'
+            numberWithCommas(totalDeaths),
+            ' American lives lost to COVID-19 since January 2020. What does this death toll actually look like?'
         );
-    
-    let dots_per_row = Math.ceil(canvasWidth/(DOT_SIZE+SPACING_SIZE));
-    let num_rows_need = (Math.floor(totalDeaths/dots_per_row));
-
-    let canvasHeight = (num_rows_need * (DOT_SIZE+SPACING_SIZE));
-    canvas.height = canvasHeight;
-
-    // console.log(num_rows_need-1);
-    // console.log(canvasHeight);
-
-    let num_dots = 0;
-
-    // rows
-    for(let y = 0; y<canvasHeight; y+= DOT_SIZE+SPACING_SIZE){
-        // cols
-        for(let x = 0; x<canvasWidth; x+= DOT_SIZE+SPACING_SIZE){
-            ctx.fillStyle = 'rgba(' + 0 + ',' + 0 + ',' + 0 + ',' + 1 + ')';
-            ctx.fillRect(x, y, DOT_SIZE, DOT_SIZE);   
-            num_dots++; 
-        }
-    }
-    // console.log(totalDeaths);
-    // console.log(num_dots);
+    monthlyDeathData.forEach(month => {
+        drawDotsOnCanvas(month.month, month.deaths);
+    });
+    // drawDotsOnCanvas("october", 20647);
+    let dots_per_row = Math.floor(document.getElementById('october').width / (DOT_SIZE + SPACING_SIZE));
+    document.getElementById("row_legend").innerHTML = 
+        "each row = ".concat(
+            dots_per_row+1,
+            " of these lives."
+        );
 });
 
+function drawDotsOnCanvas(canvasId, numDots){
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    canvas.width = canvas.clientWidth;
+    canvas.height = getHeightFromWidthAndNumDots(canvas.width, numDots);
+
+    let num_dots = 0;
+    // rows
+    for(let y = 0; y<canvas.height; y+= (DOT_SIZE+SPACING_SIZE)){
+        // cols
+        for(let x = 0; x<canvas.width; x+= (DOT_SIZE+SPACING_SIZE)){
+            if(num_dots>=numDots){
+                break;
+            }
+            ctx.fillStyle = 'rgba(' + 0 + ',' + 0 + ',' + 0 + ',' + 1 + ')';
+            ctx.fillRect(x, y, DOT_SIZE, DOT_SIZE);   
+            num_dots++;
+        }
+        
+    }   
+}
 
 function getDateStringFromDateInt(dateInt) {
     dateString = dateInt.toString();
@@ -65,4 +86,18 @@ function getDateStringFromDateInt(dateInt) {
     else {
         return "On ".concat(dateFromData.toDateString());
     }
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function getHeightFromWidthAndNumDots(width, numDots){
+    let dots_per_row = Math.ceil(width / (DOT_SIZE + SPACING_SIZE));
+    console.log(dots_per_row);
+    let num_rows_need = (Math.ceil(numDots / dots_per_row));
+
+    return (num_rows_need * (DOT_SIZE + SPACING_SIZE));
+
+
 }
